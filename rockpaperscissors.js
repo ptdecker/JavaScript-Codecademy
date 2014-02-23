a075486db97a37ce48f1c46eefdbe61c8cd36574
P. Todd Decker <ptdecker@mac.com>
2014-02-23 17:50:18 -0600

    Minor fix to computer's choice logic
    A minor fix to the bounds of the computer's choice logic so that:
    
    0.00 to 0.32 is 'rock'
    0.33 to 0.65 is 'scissors'
    0.66 to 1.00 is 'paper'
    
    TODO: Would need to look further at Math.random() to see if this could be tweaked further.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -10,9 +10,9 @@ var userChoice = prompt("Do you choose rock, paper or scissors?");
 
 var computerChoice = Math.random();
 
-if (computerChoice < 0.34) {
+if (computerChoice < 0.33) {
 	computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
+} else if(computerChoice > 0.66) {
 	computerChoice = "paper";
 } else {
 	computerChoice = "scissors";
