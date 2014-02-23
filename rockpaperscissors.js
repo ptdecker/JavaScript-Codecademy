54c3e2a5c765415177468fce51be9c118215597a
P. Todd Decker <ptdecker@mac.com>
2014-02-23 17:45:07 -0600

    A simple "Rock, Paper, Scissors" game
    The game logic in my solution doesn't follow the original outline from the Code Academy's course work; however, I think this approach which uses a helper function is easier to read than a bunch of nested if-elseif-else statements.

---
+++ rockpaperscissors.js
@@ -0,0 +1,49 @@
+/**************************************
+* Simple "Rock, Paper, Scissors" Game *
+***************************************/
+
+// Get the human player's choice
+
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+
+// Generate computer's choice
+
+var computerChoice = Math.random();
+
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
+
+// Helper function to see if the both choices match two particular values (i.e.
+// one of the choices is one value and the other choice is the other value. This
+// helps simplify and make the winning logic easier to read.
+
+var areThey = function(value1, value2, choice1, choice2) {
+    if (value1 === choice1 && value2 === choice2) {
+        return(true);
+    } else if (value1 === choice2 && value2 === choice1) {
+        return(true);
+    }
+    return(false);
+}
+
+// Determine which of the choices made is the winner or if there is a tie
+
+var compare = function(choice1, choice2) {
+    if (areThey("paper", "rock", choice1, choice2)) {
+        return("paper wins");
+    } else if (areThey("paper", "scissors", choice1, choice2)) {
+        return("scissors wins");
+    } else if (areThey("scissors", "rock", choice1, choice2)) {
+        return("rock wins");
+    }
+    return("it's a tie!");
+}
+
+// Show the results
+
+console.log(compare(userChoice, computerChoice));
