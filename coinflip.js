d68532fc373718302c4a8f462ef09726aeb3a704
P. Todd Decker <ptdecker@mac.com>
2014-02-27 10:05:53 -0600

    Added JSFiddle execution instructions

--- coinflip.js
+++ coinflip.js
@@ -2,8 +2,37 @@
 * Better coin flipper than the lesson example
 */
 
+/* To run this in JSFiddle, set HTML to:
+
+<div id="console-log"></div>
+
+CSS to:
+
+{
+.console-line
+    font-family: monospace;
+    margin: 2px;
+}
+
+And uncomment:
+
+var consoleLine = "<p class=\"console-line\"></p>";
+ 
+console = {
+    log: function (text) {
+        $("#console-log").append($(consoleLine).html(text));
+    }
+};
+
+*/
+
+// If it wasn't an excercise where Code Academy is looking for specifics,
+// I would use a Ternary instead of the if-else construct
+//
+// console.log(coinFace === 0 ? "Heads! Flipping again..." : "Tails! Done flipping.")
+
 do {
-    var coinFace = Math.floor(Math.random() *2);
+    var coinFace = Math.floor(2 * Math.random());
     if (coinFace === 0) {
         console.log("Heads! Flipping again...");
     } else {
